9fc460bedfa88a0a55faad913b151b4c7117febe
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-08 12:28:21 +0200

    created $fillable variable for data to be stored in database

--- app/Shoppingitem.php
+++ app/Shoppingitem.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shoppingitem extends Model
 {
-    //
+    protected $fillable = [
+        'shoppinglist_id',
+        'name',
+        'quantity',
+        'category'
+    ];
 }
--- app/Shoppinglist.php
+++ app/Shoppinglist.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shoppinglist extends Model
 {
-    //
+    protected $fillable = [
+        'title'
+    ];
 }
