a6ef2d2f64d1cfb0f090fcf875c30dd0c6182f3f
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-08 12:31:24 +0200

    added routes for creating shoppinglists and shoppingitems

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/shoppinglists/create', 'ShoppinglistController@create');
+Route::post('/shoppinglist/store', 'ShoppinglistController@store');
+
+Route::get('/shoppingitems/create', 'ShoppingitemController@create');
+Route::post('/shoppingitems/store', 'ShoppingitemController@store');
