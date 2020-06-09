d545757c3064e7db32191d091aa473ec213f665a
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-09 12:48:30 +0200

    added csrf + tiny layout fix

--- resources/views/shoppinglists/show.blade.php
+++ resources/views/shoppinglists/show.blade.php
@@ -3,18 +3,23 @@
 @section('content')
 
 <div class="container">
-    <div class="jumbotron col-md-8">
-        <h1 class="text-center">{{ $shoppinglist->title }}</h1>
-        <ol>
-            @foreach($shoppinglist->shoppingitems as $shoppingitem)
-            <li><br>
-                {{ __('Name: ') }} {{ $shoppingitem->name}}<br>
-                {{ __('Category: ') }}{{ $shoppingitem->category}}<br>
-                {{ __('Quantity: ') }}{{ $shoppingitem->quantity }}<hr>
-            </li>
-            @endforeach
-        </ol>
-        <small><i>{{ __('list created at: ') }}{{ $shoppinglist->created_at }}</i></small>
-    </div>
+    @include('/partials/status')
+    <form method = "Post" action ="/shoppingitem/delete">
+        <div class="jumbotron col-md-8">
+            @csrf
+            <h1 class="text-center">{{ $shoppinglist->title }}</h1>
+            <ol>
+                @foreach($shoppinglist->shoppingitems as $shoppingitem)
+                <li><br>
+                    <input type ="checkbox" name="ids[]" value="{{ $shoppingitem->id }}">&nbsp;{{ __('Name: ') }} {{ $shoppingitem->name}}<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;{{ __('Category: ') }}{{ $shoppingitem->category}}<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;{{ __('Quantity: ') }}{{ $shoppingitem->quantity }}<hr>
+                </li>
+                @endforeach
+            </ol>
+            <small><i>{{ __('list created at: ') }}{{ $shoppinglist->created_at }}</i></small>
+        </div>
+        <input type="submit" class=" btn btn-danger" value="Delete selected Item">
+    </form>
 </div>
 @endsection
