adf3f50f8d6de7fdf0028847938ad33e9d716a81
Tillmann Weimer <tillmann1970@gmail.com>
2020-06-09 11:43:28 +0200

    created view for showing Shoppinglist

--- resources/views/shoppinglists/show.blade.php
+++ resources/views/shoppinglists/show.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts/app')
 
 @section('content')
-{{ $shoppinglist->name }}
 
-
+<div class="container">
+    <div class="jumbotron col-md-8">
+        <h1 class="text-center">{{ $shoppinglist->title }}</h1>
+        <ol>
+            @foreach($shoppinglist->shoppingitems as $shoppingitem)
+            <li><br>
+                {{ __('Name: ') }} {{ $shoppingitem->name}}<br>
+                {{ __('Category: ') }}{{ $shoppingitem->category}}<br>
+                {{ __('Quantity: ') }}{{ $shoppingitem->quantity }}<hr>
+            </li>
+            @endforeach
+        </ol>
+        <small><i>{{ __('list created at: ') }}{{ $shoppinglist->created_at }}</i></small>
+    </div>
+</div>
 @endsection
